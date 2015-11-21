b0ab283aeb13b83bd8320fa49272994fce0e0ccb
Marcell Trixie Alexander <marcell.chelsea@gmail.com>
2015-11-21 00:01:53 -0800

    Update Snake.dart

--- Snake.dart
+++ Snake.dart
@@ -1,3 +1,10 @@
+/// Kelas Snake yang merupakan turunan kelas Teleporter.
+/// Kelas yang digunakan sebagai ular pada Game Ular Tangga.
 class Snake extends Teleporter{
+    
+    /// Constructor dari kelas Snake.
+    /// COnstructor kelas Snake memanggil Constructor Kelas Teleporter(Super Classnya).
+    /// [from] merupakan nilai awal atau Kepala Ular.
+    /// [to] merupakan nilai akhir atau Ekor Ular.
     Snake(int from, int to,int identifier) : super(from, to,identifier);
 }
