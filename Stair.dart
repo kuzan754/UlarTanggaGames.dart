e80b058625d05d92b79331c3a279463280153312
Marcell Trixie Alexander <marcell.chelsea@gmail.com>
2015-11-21 00:03:47 -0800

    Update Stair.dart

--- Stair.dart
+++ Stair.dart
@@ -1,3 +1,10 @@
+/// Kelas Stair yang merupakan turunan kelas Teleporter.
+/// Kelas yang digunakan sebagai Tangga pada Game Ular Tangga.
 class Stair extends Teleporter{
+    
+    /// Constructor dari kelas Stair.
+    /// Constructor kelas Stair memanggil Constructor Kelas Teleporter(Super Classnya).
+    /// [from] merupakan nilai awal atau titik awal tangga.
+    /// [to] merupakan nilai akhir atau titik akhir tangga.
     Stair(int from, int to,int identifier) : super(from, to, identifier);
 }
